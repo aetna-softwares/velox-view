b2f45b8919aeeda75c4db198f4e3a6bf6c46a84a
Olivier Oriol <olivier.oriol@aetna-softwares.com>
2017-09-03 00:02:45 +0200

    fix data update on list auto

--- ext/velox-list.js
+++ ext/velox-list.js
@@ -105,7 +105,7 @@
 
     extension.extendsObj._updateDataFromView = function (viewId, baseData, dataObject) {
         var viewData = VeloxWebView.prototype._updateDataFromView.call(this, viewId, baseData, dataObject);
-        if(this.listAutoActive && Array.isArray(viewData)){
+        if(this.views[viewId].listAutoActive && Array.isArray(viewData)){
             //remove the last entry of the array because it is always an empty line if list auto is active
             viewData.splice(viewData.length - 1, 1) ;
         }
