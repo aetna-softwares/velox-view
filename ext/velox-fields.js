d32d1ca365a37e30d1e0701cff93b1dd6dd2cddc
Olivier Oriol <olivier.oriol@aetna-softwares.com>
2017-09-07 14:48:45 +0200

    fix redispatch

--- ext/velox-fields.js
+++ ext/velox-fields.js
@@ -1100,9 +1100,11 @@
                     selectize.unlock();
                 }
             } ;
-            ["focus", "blur", "keyUp", "keyDown"].forEach(function(eventName){
+            ["change", "focus", "blur", "keyUp", "keyDown"].forEach(function(eventName){
                 $select.on(eventName, function(ev){
+                    if(ev.velox_redispatched){ return ; } //already redispatched, stop
                     var event = new Event(ev.type);
+                    event.velox_redispatched = true ;
                     //var cloneEv = new ev.constructor(ev.type, ev);
                     element.dispatchEvent(event);
                 }) ;
